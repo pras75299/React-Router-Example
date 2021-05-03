4d5997f19f323c32ba9fc59322ac1f46a9f250c9
pras75299 <singh.prashantking@gmail.com>
2021-05-03 22:52:23 +0530

    add about component

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <h2>About page</h2>
+      <br />
+      <p>This is the website using different apis with different purposes.</p>
+    </>
+  );
+}
