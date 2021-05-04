69e1ab6249469eca839fdb22050119a059125bd2
pras75299 <singh.prashantking@gmail.com>
2021-05-04 23:17:03 +0530

    correct app.test.js by replacing text Hobby by Home

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hobby/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
