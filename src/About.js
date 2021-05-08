564625e1c879463e6dfeacb2be16327d1c085181
pras75299 <singh.prashantking@gmail.com>
2021-05-08 23:25:08 +0530

    add text to about.js

--- src/About.js
+++ src/About.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <h2>About page</h2>
+      <h1 className="commontitleclass">About page</h1>
       <br />
-      <p>This is the website using different apis with different purposes.</p>
+      <p>This is a website using different apis to show information about Cats, Quotes and Harry potter characters.</p>
     </>
   );
 }
